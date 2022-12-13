9728fad54b4666b050f557f62d347f6af0332a98
Victor Whyte <victordavidwhyte@gmail.com>
2022-12-13 23:45:14 +0100

    refactor: refator common logic to npm package

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { validateRequest, BadRequestError } from '@vwtickets/common';
 
-import { validateRequest } from '../middlewares/validate-request';
 import { User } from '../models/user';
-import { BadRequestError } from '../errors/bad-request-error';
 import jwt from 'jsonwebtoken';
  
 const router = express.Router();
