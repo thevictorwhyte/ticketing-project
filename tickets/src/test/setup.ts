34e821e7345db4b420b2c4ec6befc103713ad340
Victor Whyte <victordavidwhyte@gmail.com>
2022-12-15 15:43:29 +0100

    feat: add update ticket route hander and tests

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,7 +1,5 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import request from 'supertest';
-import { app } from '../app';
 import jwt from 'jsonwebtoken';
 
 declare global {
@@ -35,7 +33,7 @@ afterAll(async () => {
 global.signin = () => {
   // Build a JWT payload {id, email}
   const payload = {
-    id: 'testid',
+    id: new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com',
   };
 
